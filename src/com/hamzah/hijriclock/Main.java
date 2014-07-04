31a5021164aaf9e3bb12554f2943b0e5bb27d94c
hamzahrmalik <hamzahrmalik@yahoo.co.uk>
2014-07-04 19:35:48 +0100

    Version 1.2
    Hijri date on new line in Notification Center

--- src/com/hamzah/hijriclock/Main.java
+++ src/com/hamzah/hijriclock/Main.java
@@ -16,7 +16,32 @@ public class Main implements IXposedHookLoadPackage{
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 		if(!lpparam.packageName.equals("com.android.systemui"))
 			return;
-		XC_MethodHook hook = new XC_MethodHook(){
+		XC_MethodHook hook_dateview = new XC_MethodHook(){
+			@Override
+			protected void afterHookedMethod(MethodHookParam param)
+					throws Throwable {
+				pref = new XSharedPreferences(this.getClass().getPackage().getName(), Keys.PREF);
+				
+				TextView t = (TextView) param.thisObject;
+				t.setSingleLine(false); //To prevent the SystemUI force stop
+				String orig =t.getText().toString();//was causing ClassCastExeption
+				String date = HijriCalendar.getDate(null, pref.getBoolean(Keys.SHOW_DATE, true),
+						pref.getBoolean(Keys.SHOW_MONTH, true), pref.getBoolean(Keys.SHOW_YEAR, true),
+						pref.getBoolean(Keys.SHOW_MONTH_AS_NUMBER, false), pref.getBoolean(Keys.SHOW_SLASH, false),
+						pref.getBoolean(Keys.USE_ARABIC_TEXT, false), pref.getBoolean(Keys.USE_ARABIC_NUMBERS, false),
+						pref.getInt(Keys.OFFSET_DAY, 0), pref.getInt(Keys.OFFSET_MONTH, 0));
+				//cut the space at the start
+				date = date.substring(1);
+				
+				if(pref.getBoolean(Keys.SHOW_BEFORE_CLOCK, false)){
+					t.setText(date+"\n"+orig);					
+				}else{
+					t.setText(orig+"\n"+date);}
+			//	OldDate=date;}
+			}
+			};
+			
+		XC_MethodHook hook_clock = new XC_MethodHook(){
 			@Override
 			protected void afterHookedMethod(MethodHookParam param)
 					throws Throwable {
@@ -37,13 +62,17 @@ public class Main implements IXposedHookLoadPackage{
 				else
 					t.append(date);
 			}
-		};
+			};
 		
-		if(Build.VERSION.SDK_INT>=14)
-			findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", hook);
+		if(Build.VERSION.SDK_INT>=14){
+			findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", hook_clock);
+			findAndHookMethod("com.android.systemui.statusbar.policy.DateView", lpparam.classLoader, "updateClock", hook_dateview);
+		}
 	       
-	    else if(Build.VERSION.SDK_INT<=13)
-	    	findAndHookMethod("com.android.systemui.statusbar.Clock", lpparam.classLoader, "updateClock", hook);
+	    else if(Build.VERSION.SDK_INT<=13){
+	    	findAndHookMethod("com.android.systemui.statusbar.Clock", lpparam.classLoader, "updateClock", hook_clock);
+	    	findAndHookMethod("com.android.systemui.statusbar.DateView", lpparam.classLoader, "updateClock", hook_dateview);
+	    }
 	}
  
 }
